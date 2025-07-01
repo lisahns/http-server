6ab19ffa2759803a4b0ff709ff873d09c3061a45
Lisa Hanes <lisa.hanes@itv.com>
2025-07-01 09:14:49 +0100

    feat: convert buffer and implement error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,62 @@
+use std::str;
+use std::str::Utf8Error;
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{ Result as FmtResult, Display, Formatter, Debug };
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // GET /search?name=abc&sort=1 HTTP/1.1
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+       
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidMethod => "InvalidMethod",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
 use std::io::Read;
 pub struct Server {
@@ -29,6 +31,11 @@ impl Server {
                         Ok(_) => {
                             // replaces invalid utf8
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            // reference to buffer, converted to array
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => print!("Failed to parse request, error: {}", e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
