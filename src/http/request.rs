a2494f38ec54cc9eaf666b0b6d01baf74fd4bbe9
Lisa Hanes <lisa.hanes@itv.com>
2025-07-03 14:48:35 +0100

    chore: converting request string

--- src/http/request.rs
+++ src/http/request.rs
@@ -17,10 +17,29 @@ impl TryFrom<&[u8]> for Request {
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
+// this is the same as matching
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+       
 
+       if protocol != "HTTP/1.1" {
+        return Err(ParseError::InvalidProtocol)
+       }
        
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            // normally we wouldn't do +1, because it is +1 byte, but we know that this space is empty, so one byte
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+    None
+    unimplemented!()
 }
 
 pub enum ParseError {
